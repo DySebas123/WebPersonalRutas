f81354e9ae9c1b7b4e9042036d1b27cce4fa0207
Dylan <dylan@gmail.com>
2026-04-19 15:24:23 -0500

    Corrigiendo error en la página Contacto

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,28 +1,29 @@
 function Contact() {
     return (
-        <div class="container col-xl-10 col-xxl-8 px-4 py-5">
-            <div class="row align-items-center g-lg-5 py-5">
-                <div class="col-lg-7 text-center text-lg-start">
-                    <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Quieres contratarme para un trabajo</h1>
-                    <p class="col-lg-10 fs-4">Que esperas!!!!, estoy buscando CHAMBA</p>
+        <div className="container col-xl-10 col-xxl-8 px-4 py-5">
+            <div className="row align-items-center g-lg-5 py-5">
+                <div className="col-lg-7 text-center text-lg-start">
+                    <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">Quieres contratarme para un trabajo</h1>
+                    <p className="col-lg-10 fs-4">Que esperas!!!!, estoy buscando CHAMBA</p>
                 </div>
-                <div class="col-md-10 mx-auto col-lg-5">
-                    <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
-                            <label for="floatingInput">Correo Electrónico</label>
+                <div className="col-md-10 mx-auto col-lg-5">
+                    <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+                        <div className="form-floating mb-3">
+                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                            <label htmlFor="floatingInput">Correo Electrónico</label>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
-                            <label for="floatingPassword">Contraseña</label>
+                        <div className="form-floating mb-3">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                            <label htmlFor="floatingPassword">Contraseña</label>
                         </div>
-                        <div class="checkbox mb-3">
-                            <label> <input type="checkbox" value="remember-me"> Recordad </input> </label>
+                        <div className="checkbox mb-3">
+                            <label> <input type="checkbox" value="remember-me"/> Recordar </label>
                         </div>
-                        <button class="w-100 btn btn-lg btn-primary" type="submit">Registrarse</button>
-                        <hr class="my-4">
-                            <small class="text-body-secondary">Al dar clic en Registrar, aceptas los términos de uso.</small>
-                        </hr>
+                        <button className="w-100 btn btn-lg btn-primary" type="submit">Registrarse</button>
+                        <hr className="my-4"/>
+                        <small className="text-body-secondary">
+                            Al dar clic en Registrar, aceptas los términos de uso.
+                        </small>
                     </form>
                 </div>
             </div>
